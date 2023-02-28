1c7f98f2ec4b9a87ad1128fa14656e3e94573f2b
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-02-27 21:28:42 -0600

    added employee types to page.js

--- src/page.js
+++ src/page.js
@@ -18,7 +18,7 @@ function renderEmployee(employee) {
         <h2>Manager</h2>
       </div>
         <ul class = "list-group">
-          <li class = "list-group-item">Name:</li>
+          <li class = "list-group-item">Name: ${manager.getName()}</li>
           <li class = "list-group-item">ID: ${manager.getId()}</li>
           <li class = "list-group-item">Email: ${manager.getEmail()}</li>
           <li class = "list-group-item">LinkedIn: ${manager.getLinkedIn()}</li>
@@ -27,6 +27,36 @@ function renderEmployee(employee) {
       break;
 
     case "Engineer":
-      
+      let engineer = new Engineer;
+      engineer.name = employee.name;
+      return `
+      <div class="card">
+      <div class="card-header">
+        <h2>Engineer</h2>
+      </div>
+      <ul class = "list-group">
+        <li class = "list-group-item">Name: ${enineer.getName()}</li>
+        <li class = "list-group-item">ID: ${engineer.getId()}</li>
+        <li class = "list-group-item">Email: ${engineer.getEmail()}</li>
+        <li class = "list-group-item">Github: ${engineer.getGithub()}</li>
+      </ul>
+    </div>`
+    break;
+
+    case "Intern":
+      let intern = new Intern;
+      intern.name = employee.name;
+      return `
+      <div class="card">
+      <div class="card-header">
+        <h2>Intern</h2>
+      </div>
+      <ul class = "list-group">
+        <li class = "list-group-item">Name: ${intern.getName()}</li>
+        <li class = "list-group-item">ID: ${intern.getId()}</li>
+        <li class = "list-group-item">Email: ${intern.getEmail()}</li>
+        <li class = "list-group-item">School: ${intern.getSchool()}</li>
+      </ul>
+    </div>`
   }
 }
